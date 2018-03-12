f567767fbf99a5cd61989861d8949a00808a5ced
Ron Kurr <kurr@kurron.org>
2018-03-12 12:40:07 -0400

    Initial compile working

---
+++ src/main/groovy/com/example/nonreactive/NonReactiveApplication.groovy
@@ -0,0 +1,12 @@
+package com.example.nonreactive
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NonReactiveApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run NonReactiveApplication, args
+	}
+}
