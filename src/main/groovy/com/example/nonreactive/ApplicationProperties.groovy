413bd86533d5fb501a649688cba20e908b2ff223
Ron Kurr <kurr@kurron.org>
2018-03-12 15:33:51 -0400

    Expand Gradle properties into the Spring configuration.

--- src/main/groovy/com/example/nonreactive/ApplicationProperties.groovy
+++ src/main/groovy/com/example/nonreactive/ApplicationProperties.groovy
@@ -1,16 +1,19 @@
 package com.example.nonreactive
 
 import groovy.transform.Canonical
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.convert.DurationUnit
 import org.springframework.validation.annotation.Validated
 
 /**
  * Type-safe configuration properties for the application.
  */
 @Canonical
-@ConfigurationProperties( prefix = 'com.example' )
+@ConfigurationProperties( prefix = 'service' )
 @Validated
 class ApplicationProperties {
 
@@ -19,6 +22,13 @@ class ApplicationProperties {
      */
     @NotNull
     UUID instance
+
+    /**
+     * How long to wait, in seconds, for downstream services.
+     */
+    @NotNull
+    @DurationUnit( ChronoUnit.SECONDS )
+    Duration timeout
 
     @Valid
     final Nested nested = new Nested()
