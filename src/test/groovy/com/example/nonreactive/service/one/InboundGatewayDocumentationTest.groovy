0407aa7065295b59e4dd091f3ea1eb54e382209b
Ron Kurr <kurr@kurron.org>
2018-03-13 14:35:52 -0400

    Added an example of documenting a REST controller.

---
+++ src/test/groovy/com/example/nonreactive/service/one/InboundGatewayDocumentationTest.groovy
@@ -0,0 +1,39 @@
+package com.example.nonreactive.service.one
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+import com.example.nonreactive.shared.ApplicationProperties
+import org.junit.experimental.categories.Category
+import org.kurron.categories.DocumentationTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+@Category( DocumentationTest )
+@WebMvcTest( controllers = [InboundGateway] )
+@AutoConfigureRestDocs
+class InboundGatewayDocumentationTest extends Specification {
+
+
+    @Autowired
+    private MockMvc mvc
+
+    @Autowired
+    private ApplicationProperties configuration
+
+    void setup() {
+        assert mvc
+        assert configuration
+    }
+
+    void documentGet() {
+        expect:
+        mvc.perform( get( '/instance' ) )
+           .andExpect( status().isOk() )
+           .andDo( document( 'get-instance' ) )
+    }
+}
